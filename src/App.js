fdc2b95e5fc0f757fffb105bd11086f7f78dd7ab
Simon <cloud9209@gmail.com>
2021-04-07 23:16:59 +0200

    connected to remote repo & firebase project. deploy complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import firebase from "firebase";
 import './App.css';
 import Main from "./Main"
+
+const firebaseConfig = {
+
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
